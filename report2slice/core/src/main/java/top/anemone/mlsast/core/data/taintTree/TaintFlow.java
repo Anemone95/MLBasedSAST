0a114bc9e3ee5729d7e6eddd3ead48e23a0cd9bb
Anemone <x565178035@126.com>
2020-02-21 12:22:59 +0800

    20/02/21 before delete taintedgem

--- report2slice/core/src/main/java/top/anemone/mlsast/core/data/taintTree/TaintFlow.java
+++ report2slice/core/src/main/java/top/anemone/mlsast/core/data/taintTree/TaintFlow.java
@@ -1,6 +1,22 @@
 package top.anemone.mlsast.core.data.taintTree;
 
-import java.util.LinkedList;
+import lombok.Data;
+import lombok.NonNull;
+import top.anemone.mlsast.core.data.Func;
 
-public class TaintFlow extends LinkedList<TaintEdge> {
+@Data
+public class TaintEdge {
+    @NonNull
+    public Func entry;
+    @NonNull
+    public Location point;
+
+    @Override
+    public String toString() {
+        if (point.startLine.equals(point.endLine)) {
+            return entry.getClazz() + "#" + entry.getMethod() + "→" + "L:" + point.startLine;
+        } else {
+            return entry.getClazz() + "#" + entry.getMethod() + "→" + "L:" + point.startLine;
+        }
+    }
 }
